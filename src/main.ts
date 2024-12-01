4ffbfd7b778f3a0dc3af6f3c9bfc136420995c58
clydeqin7 <clydeqin7@foxmail.com>
2024-12-01 09:58:09 +0800

    build: vue-router 添加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+const history = createWebHistory();
+const router = createRouter({
+  history,
+  routes: [
+    { path: '/', component: () => import('./components/HelloWorld.vue') },
+    { path: '/hello', component: () => import('./components/HelloWorld.vue') },
+  ],
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
